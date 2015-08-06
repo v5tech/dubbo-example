eea4f385b5892f081b5dc0fac03ce760cf2a191b
sxyx2008 <sxyx2008@163.com>
2015-08-06 12:04:35 +0800

    add Screenshots

--- dubbo-consumer/src/main/java/net/aimeizi/dubbo/controller/HelloController.java
+++ dubbo-consumer/src/main/java/net/aimeizi/dubbo/controller/HelloController.java
@@ -1,8 +1,10 @@
 package net.aimeizi.dubbo.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import net.aimeizi.dubbo.entity.User;
 import net.aimeizi.dubbo.service.DemoService;
 import net.aimeizi.dubbo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -12,13 +14,46 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 
+/**
+ *
+ * dubbo 消费者
+ *
+ * @Reference 注解需要在 dubbo consumer中做如下配置
+ *
+ * <dubbo:annotation/>
+ *	<context:component-scan base-package="net.aimeizi.dubbo.controller">
+ *	<context:include-filter type="annotation" expression="com.alibaba.dubbo.config.annotation.Reference"/>
+ * </context:component-scan>
+ *
+ * 若要使用@Autowired或@Resource注解需要显式声明bean
+ *
+ * 使用@Autowired或@Resource注解时需要使用dubbo:reference来声明
+ * <dubbo:reference interface="net.aimeizi.dubbo.service.UserService" id="userService"/>
+ * <dubbo:reference interface="net.aimeizi.dubbo.service.DemoService" id="demoService"/>
+ *
+ * 以上的配置均需要在spring mvc的DispatcherServlet配置中显式配置dubbo consumer的配置.如/WEB-INF/applicationContext-dubbo-consumer.xml 否则在Controller中服务报NullPointException
+ * <servlet>
+ *	<servlet-name>mvc-dispatcher</servlet-name>
+ *		<servlet-class>org.springframework.web.servlet.DispatcherServlet</servlet-class>
+ *	<init-param>
+ *	<param-name>contextConfigLocation</param-name>
+ *		<param-value>/WEB-INF/applicationContext*.xml,/WEB-INF/mvc-dispatcher-servlet.xml</param-value>
+ *	</init-param>
+ *	<load-on-startup>1</load-on-startup>
+ * </servlet>
+ *
+ */
 @Controller
 public class HelloController {
 
-	@Resource
+	@Reference
+//	@Autowired
+//	@Resource
 	private DemoService demoService;
 
-	@Resource
+	@Reference
+//	@Autowired
+//	@Resource
 	private UserService userService;
 
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
